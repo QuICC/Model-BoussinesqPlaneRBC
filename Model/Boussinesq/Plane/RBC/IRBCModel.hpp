1b8a60cd4be240cb04eaa139b638c0ea47c4ff40
Philippe Marti <philippe.marti@erdw.ethz.ch>
2024-10-10 16:41:56 +0200

    Update model to new block building

--- Model/Boussinesq/Plane/RBC/IRBCModel.hpp
+++ Model/Boussinesq/Plane/RBC/IRBCModel.hpp
@@ -1,6 +1,7 @@
-/** 
+/**
  * @file IRBCModel.hpp
- * @brief Implementation of the Boussinesq Rayleigh-Benard in a plane layer (toroidal/poloidal formulation)
+ * @brief Implementation of the Boussinesq Rayleigh-Benard in a plane layer
+ * (toroidal/poloidal formulation)
  */
 
 #ifndef QUICC_MODEL_BOUSSINESQ_PLANE_RBC_IRBCMODEL_HPP
@@ -12,10 +13,10 @@
 
 // Project includes
 //
-#include "QuICC/Simulation/Simulation.hpp"
 #include "QuICC/Generator/StateGenerator.hpp"
 #include "QuICC/Generator/VisualizationGenerator.hpp"
 #include "QuICC/Model/IPhysicalPyModel.hpp"
+#include "QuICC/Simulation/Simulation.hpp"
 
 namespace QuICC {
 
@@ -27,72 +28,74 @@ namespace Plane {
 
 namespace RBC {
 
+/**
+ * @brief Implementation of the Boussinesq Rayleigh-Benard in a plane layer
+ * (toroidal/poloidal formulation)
+ */
+class IRBCModel : public IPhysicalPyModel<Simulation, StateGenerator,
+                     VisualizationGenerator>
+{
+public:
    /**
-    * @brief Implementation of the Boussinesq Rayleigh-Benard in a plane layer (toroidal/poloidal formulation)
+    * @brief Constructor
     */
-   class IRBCModel: public IPhysicalPyModel<Simulation,StateGenerator,VisualizationGenerator>
-   {
-      public:
-         /**
-          * @brief Constructor
-          */
-         IRBCModel() = default;
+   IRBCModel() = default;
 
-         /**
-          * @brief Destructor
-          */
-         virtual ~IRBCModel() = default;
+   /**
+    * @brief Destructor
+    */
+   virtual ~IRBCModel() = default;
 
-         /// Formulation used for vector fields
-         virtual VectorFormulation::Id SchemeFormulation() override;
+   /// Formulation used for vector fields
+   virtual VectorFormulation::Id SchemeFormulation() override;
 
-         /**
-          * @brief Version string
-          */
-         std::string version() const final;
+   /**
+    * @brief Version string
+    */
+   std::string version() const final;
 
-         /**
-          * @brief Add the required equations
-          *
-          * @param spSim   Shared simulation object
-          */
-         virtual void addEquations(SharedSimulation spSim) override;
+   /**
+    * @brief Add the required equations
+    *
+    * @param spSim   Shared simulation object
+    */
+   virtual void addEquations(SharedSimulation spSim) override;
 
-         /**
-          * @brief Add the initial state generation equations
-          *
-          * @param spGen   Shared generator object
-          */
-         virtual void addStates(SharedStateGenerator spGen) override;
+   /**
+    * @brief Add the initial state generation equations
+    *
+    * @param spGen   Shared generator object
+    */
+   virtual void addStates(SharedStateGenerator spGen) override;
 
-         /**
-          * @brief Add the visualization generation equations
-          *
-          * @param spGen   Shared visualization generator
-          */
-         virtual void addVisualizers(SharedVisualizationGenerator spVis) override;
+   /**
+    * @brief Add the visualization generation equations
+    *
+    * @param spGen   Shared visualization generator
+    */
+   virtual void addVisualizers(SharedVisualizationGenerator spVis) override;
 
-         /**
-          * @brief Add the required ASCII output files
-          *
-          * @param spSim   Shared simulation object
-          */
-         virtual void addAsciiOutputFiles(SharedSimulation spSim) override;
+   /**
+    * @brief Add the required ASCII output files
+    *
+    * @param spSim   Shared simulation object
+    */
+   virtual void addAsciiOutputFiles(SharedSimulation spSim) override;
 
-         /**
-          * @brief XML configuration tags
-          */
-         virtual std::map<std::string, std::map<std::string,int> > configTags() const override;
+   /**
+    * @brief XML configuration tags
+    */
+   virtual std::map<std::string, std::map<std::string, int>>
+   configTags() const override;
 
-      protected:
+protected:
+private:
+};
 
-      private:
-   };
-
-} // RBC
-} // Plane
-} // Boussinesq
-} // Model
-} // QuICC
+} // namespace RBC
+} // namespace Plane
+} // namespace Boussinesq
+} // namespace Model
+} // namespace QuICC
 
 #endif // QUICC_MODEL_BOUSSINESQ_PLANE_RBC_IRBCMODEL_HPP
